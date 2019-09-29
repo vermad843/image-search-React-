272523fd7c6415d9a65b3c177d6ecf31bbd0cd3e
vermad843 <vishnuverma7524@gmail.com>
2019-09-29 20:58:07 +0530

    refinning the code

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,16 @@ class App extends Component {
      title : 'React Image Search',
      searchTerm : '',
      loading : false ,
-     images : []
+     images : []                           
     };
   }
 
-  formSubmitted(event) {
-    event.preventDefault();
-
-   this.setState({
-    loading : true,
-    images : []
+  formSubmitted(event) {           
+    event.preventDefault();        //when user press the button then 
+   this.setState({              
+    loading : true,               //setState is used to update the state 
+    images : []                   //when we search again something then clear the previous images only show new images.
    });
-
     API.search(this.state.searchTerm)
     .then(images => {
       this.setState({
@@ -32,29 +30,30 @@ class App extends Component {
   
   searchTermChanged(event) {
     this.setState({
-     searchTerm : event.target.value
+     searchTerm : event.target.value                    //updating as the user type in the input box;
     });
     
   }
   render() {
-    const {title , searchTerm, loading  } = this.state ;
+    const {title , searchTerm, loading ,images } = this.state ;
          return (
       <div>
         <h1>
           {title} 
         </h1>
         <form onSubmit ={this.formSubmitted.bind(this)}>
-        <label htmlFor = "searchTerm"></label>
+        <label htmlFor = "searchTerm">Search Term</label>
           <input 
             onChange = {this.searchTermChanged.bind(this)}
             value = {searchTerm}
             className = "u-full-width" 
             type = "text" 
-            id = "searchTerm"   />
+            id = "searchTerm" 
+            name = "searchTerm"  />
             <button type = "submit">Search</button>
          </form>
-         {loading ? <img src = "Blocks-1s-200px.gif" /> : '' }
-          <section className = "images">
+         {loading ? <img src = "Blocks-1s-200px.gif" /> : '' }      
+          <section className = "images">                        
              {images.map((image) => {
                return <img key ={image.id} alt ={image.description} src ={image.image_url[0]} />
              })}
@@ -64,3 +63,7 @@ class App extends Component {
   }
 }
 export default App;
+
+
+
+//using ternary operator to decide when to show the image or not and using key for identifying the image uniquely.
