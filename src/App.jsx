90715c33035212f122d0b25520a5438759d3ddbd
vermad843 <vishnuverma7524@gmail.com>
2019-09-29 11:31:07 +0530

    adding searchTermChanged method and handling setState is undefined error

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
+import { tsConstructorType } from '@babel/types';
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+     title : 'React Image Search',
+     searchTerm : '' 
+    }
+  }
+
+  
+  searchTermChanged(event) {
+    console.log(event.target.value);
+    this.setState({
+     searchTerm : event.target.value
+    });
+    
+  }
+
+
+
+
+
   render() {
+    const {title , searchTerm} = this.state ;
     return (
       <div>
         <h1>
-          React Image App 
+          {title} 
         </h1>
+        <form>
         <label htmlFor = "searchTerm"></label>
-        <input 
-         className = "u-full-width" 
-         type = "text" 
-         id = "searchTerm"   />
-         <button type = "submit">Search</button>
+          <input 
+            onChange = {this.searchTermChanged.bind(this)}
+            value = {searchTerm}
+            className = "u-full-width" 
+            type = "text" 
+            id = "searchTerm"   />
+            <button type = "submit">Search</button>
+         </form>
       </div>
     );
   }
